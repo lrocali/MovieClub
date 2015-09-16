b595fe61f80c86c14d63581ea5037703f62941a6
Lucas Rocali <s3495204@student.rmit.edu.au>
2015-09-17 04:59:36 +1000

    Next step put search view to close/change

--- app/src/main/java/com/example/rocali/movieclub/Controllers/MovieList.java
+++ app/src/main/java/com/example/rocali/movieclub/Controllers/MovieList.java
@@ -1,14 +1,23 @@
 package com.example.rocali.movieclub.Controllers;
 
+import android.app.ActionBar;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.example.rocali.movieclub.Models.JsonClass;
@@ -35,31 +44,79 @@ public class MovieList extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
 
+        //ActionBar actionBar = getActionBar();
+        //Log.v(TAG,actionBar.getTitle().toString());
+        // Enabling Back navigation on Action Bar icon
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //txtQuery = (TextView) findViewById(R.id.txtQuery);
+
+        //handleIntent(getIntent());
+
+
         if (model.isNetworkConnectionAvailable(this)) {
             Log.v(TAG, "CONECTED");
             model.searchMovie("http://www.omdbapi.com/?t=Fight+Club&y=&plot=short&r=json");
             //Toast.makeText(getApplicationContext(), "File already exist under SD card, playing Music", Toast.LENGTH_LONG).show();
         }
-            Log.v(TAG,"NOT CONECTED");
+            Log.v(TAG, "NOT CONECTED");
 
     }
     @Override
     public void onResume() {
         super.onResume();
+        populateListView(false);
+    }
+
+    public void populateListView(boolean searching) {
         //populate list view
         ListView lv = getListView();
-        lv.setAdapter(new CustomAdapter(this));
+        if (searching) {
+            Toast.makeText(this, "SEARCHING", Toast.LENGTH_LONG).show();
+            String[] values = new String[] { "Android List View",
+                    "Adapter implementation",
+                    "Simple List View In Android",
+                    "Create List View Android",
+                    "Android Example",
+                    "List View Source Code",
+                    "List View Array Adapter",
+                    "Android Example List View"
+            };
 
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                Intent i = new Intent(getApplicationContext(), MovieSelected.class);
-                // sending movie id to new activity
-                i.putExtra("movieId", position + "");
-                startActivity(i);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
-            }
-        });
+            // Assign adapter to ListView
+            lv.setAdapter(adapter);
+
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+
+                   /* Intent i = new Intent(getApplicationContext(), MovieSelected.class);
+                    // sending movie id to new activity
+                    i.putExtra("movieId", position + "");
+                    startActivity(i);*/
+
+                }
+            });
+        } else {
+            Toast.makeText(this, "NOT SEARCHING", Toast.LENGTH_LONG).show();
+
+            lv.setAdapter(new CustomAdapter(this));
+
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    Intent i = new Intent(getApplicationContext(), MovieSelected.class);
+                    // sending movie id to new activity
+                    i.putExtra("movieId", position + "");
+                    startActivity(i);
+
+                }
+            });
+        }
+
     }
 
     // Async Task Class
@@ -101,7 +158,7 @@ public class MovieList extends ListActivity {
             //Toast.makeText(getApplicationContext(), "Download complete, playing Music", Toast.LENGTH_LONG).show();
             // Play the music
             // playMusic();
-            Log.v(TAG,"POST EXECUTE");
+            Log.v(TAG, "POST EXECUTE");
             String title = null;
             try {
                 title = movie.getString("Title");
@@ -112,6 +169,92 @@ public class MovieList extends ListActivity {
             }
 
         }
+
     }
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        // Inflate menu to add items to action bar if it is present.
+        inflater.inflate(R.menu.menu_main, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }*/
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+
+            //txtQuery.setText("Search Query: " + query);
+
+        } else {
+            populateListView(false);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_movie_list, menu);
+
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        SearchView.OnQueryTextListener textChangeListener = new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // this is your adapter that will be filtered
+                //myAdapter.getFilter().filter(newText);
+
+                Log.v(TAG, "on text chnge text: " + newText);
+                //Toast.makeText(getApplicationContext(), "CHANGE", Toast.LENGTH_SHORT).show()
+
+                populateListView(true);
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // this is your adapter that will be filtered
+                //myAdapter.getFilter().filter(query);
+                //populateListView(false);
+                Log.v(TAG, "on query submit: " + query);
+                return true;
+            }
+        };
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+
+            @Override
+            public boolean onClose() {
+                populateListView(false);
+                Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+
+        searchView.setOnQueryTextListener(textChangeListener);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
 
 }
